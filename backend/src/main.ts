8bab91c886bea8e39455b818d4a176b67e98b1fb
phall168k <phall168k@gmail.com>
2026-09-23 10:42:24 +0700

    chore(backend): configure global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(process.env.API_PREFIX ?? 'api');
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
