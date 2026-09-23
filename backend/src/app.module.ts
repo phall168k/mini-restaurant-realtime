6613a2179e57690714623e9be10f75931647f089
phall168k <phall168k@gmail.com>
2026-09-23 11:22:42 +0700

    chore(backend): add root modules container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: '.env',
     }),
     DatabaseModule,
+    ModulesModule,
   ],
   controllers: [],
   providers: [],
