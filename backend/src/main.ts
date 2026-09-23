48a35a0aef8f561dbcb6324bfc4c1b2863158e48
phall168k <phall168k@gmail.com>
2026-09-23 10:44:24 +0700

    chore(backend): enable versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
