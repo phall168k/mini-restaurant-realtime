d7a7bc2c1fbd94a885f63351eb4df5e3b35f51f9
phall168k <phall168k@gmail.com>
2026-09-23 11:17:14 +0700

    feat(database): create admin schema and enable uuid extension

---
+++ backend/src/database/migrations/admin/1788771942538-create-admin-schema.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+const schemaName = 'admin';
+
+export class CreateAdminSchema1790136988669 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createSchema(schemaName, true);
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropSchema(schemaName, true, true);
+    }
+
+}
