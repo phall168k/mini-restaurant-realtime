ed858cf888c7f284e39a0e6abb2cecb17f19bd17
phall168k <phall168k@gmail.com>
2026-09-23 10:33:38 +0700

    chore(config): configure global API prefix from environment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(process.env.API_PREFIX ?? 'api');
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
