db6927a902a82471beb5fd21069c710e1e71d5a1
phall168k <phall168k@gmail.com>
2026-09-23 10:50:46 +0700

    feat(backend): add HTTP response interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { HttpResponseInterceptor } from './libs/http/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  app.useGlobalInterceptors(new HttpResponseInterceptor());
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
