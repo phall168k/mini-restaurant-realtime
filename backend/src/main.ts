516bb8448f36a92255ef57f4fc23306605b53f6a
phall168k <phall168k@gmail.com>
2026-09-23 10:34:40 +0700

    chore(backend): enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(process.env.API_PREFIX ?? 'api');
+  app.enableCors();
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
